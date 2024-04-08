1d8aef19972daa2bb8c4b5c6cd0fcb74ef635c55
ZarRza <160187613+ZarRza@users.noreply.github.com>
2024-04-08 18:51:27 +0430

    Created models.py and admin.py

--- Tracker/models.py
+++ Tracker/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class TaskPyTrack(models.Model):
+    PRIORITY_CHOICES = (
+        (1 , 'Emergency'),
+        (2 , 'High'),
+        (3 , 'Medium'),
+        (4 , 'Low'),
+    )
+    Task = models.CharField(max_length=255, blank=True)
+    slug = models.CharField(max_length=255, blank=True)
+    Description = models.TextField(blank=True)
+    Init_Date = models.CharField(max_length=10, blank=True)
+    Complete_Date = models.CharField(max_length=10, blank=True)
+    Deadline_Date = models.CharField(max_length=10, blank=True)
+    File = models.FileField(upload_to='pk-slug', blank=True)
+    Priority = models.PositiveSmallIntegerField(choices= PRIORITY_CHOICES , blank=True , null =True )
+    Note = models.TextField(blank=True)
+    Is_Complete = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Task'
+        order_with_respect_to = '-Priority'
+
+    def __str__(self):
+        return self.pk + '-' + str(self.Task)
