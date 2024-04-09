e728c4dd5821e79a36f9f30c3e19f3f05c2c162f
ZarRza <160187613+ZarRza@users.noreply.github.com>
2024-04-10 01:05:03 +0430

    Change name`s TaskpyTrack model & Ømvement  Meta class

--- Tracker/models.py
+++ Tracker/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
-class TaskPyTrack(models.Model):
+
+class Task(models.Model):
+    class Meta:
+        verbose_name_plural = 'Tasks'
+
     PRIORITY_CHOICES = (
-        (1 , 'Emergency'),
-        (2 , 'High'),
-        (3 , 'Medium'),
-        (4 , 'Low'),
+        (1, 'Emergency'),
+        (2, 'High'),
+        (3, 'Medium'),
+        (4, 'Low'),
     )
     Task = models.CharField(max_length=255, blank=True)
     slug = models.CharField(max_length=255, blank=True)
@@ -14,13 +18,9 @@ class TaskPyTrack(models.Model):
     Complete_Date = models.CharField(max_length=10, blank=True)
     Deadline_Date = models.CharField(max_length=10, blank=True)
     File = models.FileField(upload_to='pk-slug', blank=True)
-    Priority = models.PositiveSmallIntegerField(choices= PRIORITY_CHOICES , blank=True , null =True )
+    Priority = models.PositiveSmallIntegerField(choices=PRIORITY_CHOICES, blank=True, null=True)
     Note = models.TextField(blank=True)
     Is_Complete = models.BooleanField(default=False)
-
-    class Meta:
-        verbose_name_plural = 'Task'
-        order_with_respect_to = '-Priority'
 
     def __str__(self):
         return self.pk + '-' + str(self.Task)
