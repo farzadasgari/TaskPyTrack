20e052c367bd4cc55403f9bde4158b4472e97d2f
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-04-11 15:59:59 +0330

    Add task completed

--- TaskPyTrack/urls.py
+++ TaskPyTrack/urls.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import re_path, include
 
 urlpatterns = [
-    # re_path('^admin/', admin.site.urls),
+    re_path('^admin/', admin.site.urls),
     re_path(r'', include('Tracker.urls'))
 ]
--- Tracker/views.py
+++ Tracker/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+
+from django.shortcuts import render, redirect
+from .models import Task as TaskModel
 
 
 def index(request):
-    return render(request, 'Index.html')
+    tasks = TaskModel.objects.all()
+    if request.method == "POST":
+        Task = request.POST.get('Task')
+        Description = request.POST.get('Description')
+        Priority = request.POST.get('Priority')
+        NewTask = TaskModel(Task=Task, Description=Description, Priority=Priority,
+                            Init_Date=str(datetime.now())[:10].replace('-', '/'),
+                            Deadline_Date=str(datetime.now() + timedelta(days=5))[:10].replace('-', '/'))
+        NewTask.save()
+        return redirect('index')
+    return render(request, 'Index.html', {'tasks': tasks})
