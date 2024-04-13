00655405fc8901e6670588e5433f9899d64ca9e7
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-04-13 21:01:52 +0330

    The site template and its back-end have been completed (#4)

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -1,8 +1,12 @@
 from django.urls import re_path
 from .views import *
 
-
 urlpatterns = [
     # re_path('^admin/', admin.site.urls),
     re_path(r'^$', index, name='index'),
+    re_path(r'^delete_task/(?P<pk>\d+)$', delete_task, name='delete_task'),
+    re_path(r'^search_task/$', search_task, name='search_task'),
+    re_path(r'^edit_task/(?P<pk>\d+)$', edit_task, name='edit_task'),
+    re_path(r'^complete_task/(?P<pk>\d+)$', complete_task, name='complete_task'),
+
 ]
--- Tracker/views.py
+++ Tracker/views.py
@@ -1,5 +1,69 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from .models import Task as TaskModel, Track
 
 
 def index(request):
-    return render(request, 'Index.html')
+    tracks = Track.objects.order_by('-Checklist__Init_Date')
+    if request.method == "POST":
+        Task = request.POST.get('Task')
+        Description = request.POST.get('Description')
+        Priority = request.POST.get('Priority')
+        Deadline_Date = request.POST.get('Deadline_Date')
+        NewTask = TaskModel(Task=Task, Description=Description, Priority=Priority,
+                            Init_Date=str(datetime.now())[:10].replace('-', '/'),
+                            Deadline_Date=Deadline_Date)
+        NewTask.save()
+        NewTrack = Track(Checklist=NewTask)
+        NewTrack.save()
+        return redirect('index')
+    return render(request, 'Index.html', {'tracks': tracks})
+
+
+def delete_task(request, pk):
+    track = Track.objects.get(pk=pk)
+    track.delete()
+    return redirect('index')
+
+
+def search_task(request):
+    if request.method == "POST":
+        search = request.POST.get('search')
+        tracks = Track.objects.filter(
+            Q(Checklist__Task__icontains=search) | Q(Checklist__Description__icontains=search))
+        return render(request, 'Index.html', {'tracks': tracks, 'search': search})
+
+
+def edit_task(request, pk):
+    if request.method == "POST":
+        MyTrack = Track.objects.get(pk=pk)
+        Edit_Task = request.POST.get('Edit_Task')
+        Edit_Description = request.POST.get('Edit_Description')
+        Edit_Priority = request.POST.get('Edit_Priority')
+        Edit_Deadline_Date = request.POST.get('Edit_Deadline_Date')
+        Edit_Note = request.POST.get('Edit_Note')
+        Edit_File = request.FILES.get('Edit_File')
+        MyTrack.Checklist.Description = Edit_Description
+        MyTrack.Checklist.Priority = Edit_Priority
+        MyTrack.Checklist.Deadline_Date = Edit_Deadline_Date
+        MyTrack.Checklist.Task = Edit_Task
+        MyTrack.Checklist.save()
+        MyTrack.Note = Edit_Note
+        if Edit_File:
+            MyTrack.File = Edit_File
+        MyTrack.save()
+        return redirect('index')
+
+
+def complete_task(request, pk):
+    track = Track.objects.get(pk=pk)
+    if track.Checklist.Is_Complete:
+        track.Checklist.Is_Complete = False
+    else:
+        track.Checklist.Is_Complete = True
+    track.Checklist.save()
+    track.Complete_Date = str(datetime.now())[:10].replace('-', '/')
+    track.save()
+    return redirect('index')
