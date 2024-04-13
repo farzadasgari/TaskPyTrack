7441eb58787ce4bfc719c39bbe5b6a7311c9a4d0
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-04-13 03:57:58 +0330

    Complet section was completed, front design and task editing were improved

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     re_path(r'^delete_task/(?P<pk>\d+)$', delete_task, name='delete_task'),
     re_path(r'^search_task/$', search_task, name='search_task'),
     re_path(r'^edit_task/(?P<pk>\d+)$', edit_task, name='edit_task'),
+    re_path(r'^complete_task/(?P<pk>\d+)$', complete_task, name='complete_task'),
 
 ]
--- Tracker/views.py
+++ Tracker/views.py
@@ -31,21 +31,36 @@ def delete_task(request, pk):
 def search_task(request):
     if request.method == "POST":
         search = request.POST.get('search')
-        tracks = Track.objects.filter(Q(Checklist__Task__icontains=search) | Q(Checklist__Description__icontains=search))
+        tracks = Track.objects.filter(
+            Q(Checklist__Task__icontains=search) | Q(Checklist__Description__icontains=search))
         return render(request, 'Index.html', {'tracks': tracks, 'search': search})
 
 
 def edit_task(request, pk):
     if request.method == "POST":
+        MyTrack = Track.objects.get(pk=pk)
         Edit_Task = request.POST.get('Edit_Task')
         Edit_Description = request.POST.get('Edit_Description')
         Edit_Priority = request.POST.get('Edit_Priority')
         Edit_Deadline_Date = request.POST.get('Edit_Deadline_Date')
-        MyTrack = Track.objects.get(pk=pk)
+        Edit_Note = request.POST.get('Edit_Note')
         MyTrack.Checklist.Description = Edit_Description
         MyTrack.Checklist.Priority = Edit_Priority
         MyTrack.Checklist.Deadline_Date = Edit_Deadline_Date
         MyTrack.Checklist.Task = Edit_Task
         MyTrack.Checklist.save()
+        MyTrack.Note = Edit_Note
         MyTrack.save()
         return redirect('index')
+
+
+def complete_task(request, pk):
+    track = Track.objects.get(pk=pk)
+    if track.Checklist.Is_Complete:
+        track.Checklist.Is_Complete = False
+    else:
+        track.Checklist.Is_Complete = True
+    track.Checklist.save()
+    track.Complete_Date = str(datetime.now())[:10].replace('-', '/')
+    track.save()
+    return redirect('index')
