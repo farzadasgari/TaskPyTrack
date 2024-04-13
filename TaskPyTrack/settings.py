cab68e78c95ecc6fbb536b438c316dce7acf0194
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-04-13 14:05:32 +0330

    App admin was removed from the setting file

--- TaskPyTrack/settings.py
+++ TaskPyTrack/settings.py
@@ -17,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
 
     'Tracker.apps.TrackerConfig',
 ]
